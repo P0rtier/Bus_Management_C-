acfbb0c2813cb80ed01727a0a399f7016cf76b77
Jakub Michalski <jakub.michalski2000@outlook.com>
2023-01-03 16:15:08 +0100

    Implement remaining datagrids on kierowca screen

--- bd2_proj/Kierowca.cs
+++ bd2_proj/Kierowca.cs
@@ -8,18 +8,51 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using MySql.Data.MySqlClient;
+using MySqlX.XDevAPI.Relational;
 
 namespace bd2_proj
 {
     public partial class Kierowca : Form
     {
+        MySqlConnection connection;
+
         public Kierowca()
         {
             InitializeComponent();
 
-            string connection = "datasource=localhost;port=3306;username=Kierowca;password=123";
-            string table = "kierowca_rozklad_jazdy_administratora_view";
-            this.rozkladAdministratora1.init(new MySqlConnection(connection), table);
+            this.connection = new MySqlConnection("datasource=localhost;port=3306;username=Kierowca;password=123");
+            this.rozkladAdministratora1.init(connection, "kierowca_rozklad_jazdy_administratora_view");
+
+            updateBrygadaDataGrid();
+            updateBrygadzistaTimetableDataGrid();
+        }
+
+        private void updateBrygadaDataGrid()
+        {
+            updateDataGrid(dataGridView1, "kierowca_brygada_view");
+        }
+
+        private void updateBrygadzistaTimetableDataGrid()
+        {
+            updateDataGrid(dataGridView3, "kierowca_rozklad_jazdy_brygadzisty_view");
+        }
+
+        private void updateDataGrid(DataGridView dgrid, string table)
+        {
+            try
+            {
+                string query = "select * from `mpk_bd2`.`" + table + "`";
+                MySqlCommand command = new MySqlCommand(query, connection);
+                MySqlDataAdapter mySqlAdapter = new MySqlDataAdapter();
+                mySqlAdapter.SelectCommand = command;
+                DataTable dTable = new DataTable();
+                mySqlAdapter.Fill(dTable);
+                dgrid.DataSource = dTable;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
         }
     }
 }
