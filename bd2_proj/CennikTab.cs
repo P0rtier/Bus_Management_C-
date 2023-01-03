8f23e72ae7a46d8f3e030a82303e16409d6cdc30
Patryk Uzarowski <patrykw595@gmail.com>
2023-01-03 16:20:43 +0100

    [Cennik controll fill]

--- bd2_proj/CennikTab.cs
+++ bd2_proj/CennikTab.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using MySql.Data.MySqlClient;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -10,11 +11,35 @@ using System.Windows.Forms;
 
 namespace bd2_proj
 {
+
     public partial class CennikTab : UserControl
     {
+        private string connection = "datasource=localhost;port=3306;username=Administrator;password=123";
+        private string table = "cennik";
+
+        private void updateCennikGrid()
+        {
+            try
+            {
+                string Query = "select * from `mpk_bd2`.`" + table + "`";
+                MySqlConnection MpkBdConnection = new MySqlConnection(connection);
+                MySqlCommand MyCommand = new MySqlCommand(Query, MpkBdConnection);
+                MySqlDataAdapter MyAdapter = new MySqlDataAdapter();
+                MyAdapter.SelectCommand = MyCommand;
+                DataTable dTable = new DataTable();
+                MyAdapter.Fill(dTable);
+                dataGridView1.DataSource = dTable;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
         public CennikTab()
         {
             InitializeComponent();
+            updateCennikGrid();
         }
     }
 }
