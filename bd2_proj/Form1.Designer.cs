91b4e682e579ea660a530e42d626290ecf44fd5a
Jakub Michalski <jakub.michalski2000@outlook.com>
2023-01-15 18:27:06 +0100

    Hide password in login text field

--- bd2_proj/Form1.Designer.cs
+++ bd2_proj/Form1.Designer.cs
@@ -48,6 +48,7 @@
             // 
             this.textBox2.Location = new System.Drawing.Point(63, 43);
             this.textBox2.Name = "textBox2";
+            this.textBox2.PasswordChar = '*';
             this.textBox2.Size = new System.Drawing.Size(154, 23);
             this.textBox2.TabIndex = 2;
             // 
